f7a2f8032ea2c94eee5a1b1929698bd3e0e79e40
Arjen van Bochoven <arjen@bochoven.net>
2014-04-11 23:19:22 +0200

    Added data tables model and controller

---
+++ app/controllers/datatables.php
@@ -0,0 +1,92 @@
+<?php
+class datatables extends Controller
+{
+	function __construct()
+	{
+		if( ! $this->authorized())
+		{
+			die('Authenticate first.'); // Todo: return json?
+		}
+	} 
+
+	function data()
+	{
+		// Sanitize the GET variables here.
+		$cfg = array(
+			'iDisplayStart' => 0, // Start
+			'iDisplayLength' => 0, // Length
+			'sEcho' => 0, // Identifier, just return
+			'iColumns' => 0, // Number of columns
+			'iSortingCols' => 0, // Amount of sort columns
+			'sSearch' => '' // Search query
+		);
+
+		$cols = $sortcols = $searchcols = array();
+
+		// Process $_GET array
+		foreach($_GET as $k => $v)
+		{
+			if(isset($cfg[$k]))
+			{
+				$cfg[$k] = $v;
+			}
+			elseif(preg_match('/^mDataProp_(\d+)/', $k, $matches))
+			{
+				// Get colname from mDataProp_xx
+				$cols[$matches[1]] = $v;
+			}
+			elseif(preg_match('/^iSortCol_(\d+)/', $k, $matches))
+			{
+				$col = $matches[1];
+				if( ! isset($_GET["bSortable_$v"]))
+					continue;
+
+				if($_GET["bSortable_$v"] == "true")
+                {
+                    $sortcols[$v] = 'DESC';
+                    if(isset($_GET["sSortDir_$col"]))
+                    {
+                    	if($_GET["sSortDir_$col"] === 'asc')
+                    	{
+                    		$sortcols[$v] = 'ASC';
+                    	}
+                    }
+                }
+            }
+            elseif(preg_match('/^sSearch_(\d+)/', $k, $matches))
+            {
+            	$col = $matches[1];
+				if( ! isset($_GET["bSearchable_$col"]))
+					continue;
+
+				if($_GET["bSearchable_$col"] == "true" && $v)
+                {
+                    $searchcols[$col] = $v;
+                }
+            }
+		}
+
+		// Add columns to config
+		$cfg['cols'] = $cols;
+		$cfg['sort_cols'] = $sortcols;
+		$cfg['search_cols'] = $searchcols;
+
+		//print_r($cfg);
+
+		try
+		{
+			// Get model
+			$obj = new Tablequery($cfg);
+			echo json_encode($obj->fetch($cfg));
+		}
+		catch(Exception $e) 
+		{
+			echo json_encode(array(
+				'error' => $e->getMessage(),
+				'sEcho' => intval($cfg['sEcho'])
+			));
+		}
+
+	}
+	
+}
---
+++ app/models/tablequery.php
@@ -0,0 +1,200 @@
+<?php
+
+class Tablequery {
+    
+    private $cfg = array();
+
+    function __construct($cfg='')
+    {
+        $this->cfg = $cfg;
+    }
+    
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Retrieve all entries for serial
+	 *
+	 * @param string serial
+	 * @return array
+	 * @author abn290
+	 **/
+    function fetch($cfg)
+    {
+        $dbh = getdbh();
+
+        // Initial value
+        $iTotal = 0;
+
+        // Get tables from column names
+        $tables = array();
+        $formatted_columns = array();
+        foreach($cfg['cols'] AS $pos => $name)
+        {
+            $tbl_col_array = explode('#', $name);
+            if(count($tbl_col_array) == 2)
+            {
+                // Store table name
+                $tables[$tbl_col_array[0]] = 1;
+                // Format column name
+                $formatted_columns[$pos] = sprintf('`%s`.`%s`', 
+                    $tbl_col_array[0], $tbl_col_array[1]);
+            }
+            else
+            {
+                $formatted_columns[$pos] = sprintf('`%s`', $name);
+            }
+        }
+
+        // Select
+        $select = "SELECT ".implode(',', $formatted_columns);
+
+        $tbl_list = array_keys($tables);
+
+        // From
+        $from = 'FROM ' . array_shift($tbl_list);
+
+        // Join
+        foreach($tbl_list as $name)
+        {
+            $from .= " LEFT JOIN $name USING (serial_number)";
+        }
+
+        // Get total records
+        $sql = "
+            SELECT COUNT(1) as count
+            $from";
+        if( ! $stmt = $dbh->prepare( $sql ))
+        {
+            $err = $dbh->errorInfo();
+            die($err[2]);
+        }
+        $stmt->execute();// $bindings );
+        if( $rs = $stmt->fetch( PDO::FETCH_OBJ ) )
+        {
+            $iTotal = $rs->count;
+        }   
+
+        // Paging
+        $sLimit = sprintf(' LIMIT %d,%d', 
+            $cfg['iDisplayStart'], $cfg['iDisplayLength']);
+
+        // Show all
+        if( $cfg['iDisplayLength'] == -1 )
+        {
+            $sLimit = '';
+        }
+
+        // Ordering
+        $sOrder = "";
+        if(count($cfg['sort_cols']) > 0)
+        {
+            $sOrder = "ORDER BY  ";
+            $order_arr = array();
+            foreach($cfg['sort_cols'] AS $pos => $direction)
+            {
+                $order_arr[] = sprintf('%s %s', $formatted_columns[$pos], $direction);
+            }
+            $sOrder = "ORDER BY  ".implode(',', $order_arr);
+        }
+
+        // Search
+        $sWhere = "";
+        if($cfg['sSearch'])
+        {
+            $sWhere = "WHERE (";
+            foreach($formatted_columns AS $col)
+            {
+                $sWhere .= $col." LIKE '%".( $cfg['sSearch'] )."%' OR ";
+            }
+            $sWhere = substr_replace( $sWhere, "", -3 );
+            $sWhere .= ')';
+        }
+
+        // Search columns
+        if($cfg['search_cols'])
+        {
+            $sWhere = "WHERE (";
+            foreach ($cfg['search_cols'] as $pos => $val)
+            {
+                if(is_string($val))
+                {
+                    if(preg_match('/([<>=] \d+)|BETWEEN\s+\d+\s+AND\s+\d+$/', $val))
+                    {
+                        // Special case, use unquoted
+                        $compstr = $val;
+                    }
+                    else
+                    {
+                        // Regular string, quote
+                        $compstr = " = '$val'";
+                    }
+                }
+                else // Integer or boolean
+                {
+                    $compstr = " = $val";
+                }
+                $sWhere .= $formatted_columns[$pos].$compstr." OR ";
+            }
+            $sWhere = substr_replace( $sWhere, "", -3 );
+            $sWhere .= ')';
+        }
+
+        // Get filtered results count
+        $iFilteredTotal = $iTotal;
+        if( $sWhere)
+        {
+            $sql = "
+                SELECT COUNT(*) as count
+                $from
+                $sWhere";
+            if( ! $stmt = $dbh->prepare( $sql ))
+            {
+                $err = $dbh->errorInfo();
+				die($err[2]);
+            }
+            $stmt->execute();// $bindings );
+            if( $rs = $stmt->fetch( PDO::FETCH_OBJ ) )
+            {
+                $iFilteredTotal = $rs->count;
+            }   
+        }
+        
+        $output = array(
+            "sEcho" => intval($cfg['sEcho']),
+            "iTotalRecords" => $iTotal,
+            "iTotalDisplayRecords" => $iFilteredTotal,
+            "aaData" => array()
+        );
+
+
+        $sql = "
+        $select
+        $from
+        $sWhere
+        $sOrder
+        $sLimit
+        ";
+
+        // When in debug mode, send sql as well
+        if(conf('debug'))
+        {
+            $output['sql'] = str_replace("\n", '', $sql);
+        }
+        
+        if( ! $stmt = $dbh->prepare( $sql ))
+        {
+            $err = $dbh->errorInfo();
+			die($err[2]);
+        }
+        $stmt->execute();// $bindings );
+        $arr=array();
+        while ( $rs = $stmt->fetch( PDO::FETCH_NUM ) )
+        {
+            $output['aaData'][] = array_combine($cfg['cols'], $rs);
+        }        
+
+        return $output;
+        
+    }
+
+}
