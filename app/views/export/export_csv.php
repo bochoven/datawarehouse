d1d155dde19322ba9f6e283126811a7639f57115
Arjen van Bochoven <arjen@bochoven.net>
2014-06-15 17:45:30 +0200

    Added calf streamfilter for export_csv

--- app/views/export/export_csv.php
+++ app/views/export/export_csv.php
@@ -3,8 +3,28 @@
 
 	header( 'Content-Type: text/csv' );
 	header( 'Content-Disposition: attachment;filename='.$filename);
+
+	// filter class that applies CRLF line endings
+	class crlf_filter extends php_user_filter
+	{
+	    function filter($in, $out, &$consumed, $closing)
+	    {
+	        while ($bucket = stream_bucket_make_writeable($in)) {
+	            // make sure the line endings aren't already CRLF
+	            $bucket->data = preg_replace("/(?<!\r)\n/", "\r\n", $bucket->data);
+	            $consumed += $bucket->datalen;
+	            stream_bucket_append($out, $bucket);
+	        }
+	        return PSFS_PASS_ON;
+	    }
+	}
+	// register the filter
+	stream_filter_register('crlf', 'crlf_filter');
 	
 	$fp = fopen('php://output', 'w');
+
+	// Attach stream filter that converts lf to crlf
+	stream_filter_append($fp, 'crlf');
 
 	$fixed = new Fixed();
 
