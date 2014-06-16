e8a627838f876f809ce7a392e6d313b9b6296d8c
Arjen van Bochoven <arjen@bochoven.net>
2014-06-16 22:50:16 +0200

    Simplify export_csv

--- app/views/export/export_csv.php
+++ app/views/export/export_csv.php
@@ -3,30 +3,13 @@
 
 	header( 'Content-Type: text/csv' );
 	header( 'Content-Disposition: attachment;filename='.$filename);
-
-	// filter class that applies CRLF line endings
-	class crlf_filter extends php_user_filter
-	{
-	    function filter($in, $out, &$consumed, $closing)
-	    {
-	        while ($bucket = stream_bucket_make_writeable($in)) {
-	            // make sure the line endings aren't already CRLF
-	            $bucket->data = preg_replace("/(?<!\r)\n/", "\r\n", $bucket->data);
-	            $consumed += $bucket->datalen;
-	            stream_bucket_append($out, $bucket);
-	        }
-	        return PSFS_PASS_ON;
-	    }
-	}
-	// register the filter
-	stream_filter_register('crlf', 'crlf_filter');
 	
 	$fp = fopen('php://output', 'w');
 
-	// Attach stream filter that converts lf to crlf
-	stream_filter_append($fp, 'crlf');
-
 	$fixed = new Fixed();
+
+	// No quoting allowed around values, so a simple implode
+	// will be enough
 
 	$stmt = $fixed->prepare( $sql );
 	$fixed->execute( $stmt );
@@ -35,10 +18,10 @@
 	{
 		if(! $header)
 		{
-			fputcsv($fp, array_keys($row), ';', '"');
+			fwrite($fp, implode(';', array_keys($row))."\r\n");
 			$header = TRUE;
 		}
-		fputcsv($fp, $row, ';', '"');
+		fwrite($fp, implode(';', $row)."\r\n");
 	}
 
 	fclose($fp);
