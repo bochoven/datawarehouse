91fd4b173d57db4860f93919a246b85f1445e580
Arjen van Bochoven <arjen@bochoven.net>
2015-08-28 18:39:26 +0200

    Added tooltips

--- app/views/fixes/hardware.php
+++ app/views/fixes/hardware.php
@@ -26,6 +26,7 @@ new Fixed;
                 <th data-colname='topdesk.objecttype'>Type</th>
                 <th data-colname='topdesk.rm_specification'>Specificatie</th>
                 <th data-colname='topdesk.serienummer'>Serienummer</th>
+                
                 <th data-colname='topdesk.budgethouderid_naam'>Faculteit-Dienst</th>
                 <th data-hide="1" data-colname='fixed.budgethouderid_naam'>Faculteit-Dienst</th>
 
@@ -37,19 +38,25 @@ new Fixed;
                 <th data-hide="1" data-colname='fixed.vrijegetal1'>Abonnementsprijs</th>
 
                 <th data-colname='topdesk.vestigingid_naam'>Gebouw</th>
-                <th data-colname='fixed.lokatieid_naam'>Kamer</th>
+                
+                <th data-colname='topdesk.lokatieid_naam'>Kamer</th>
+                <th data-hide="1" data-colname='fixed.lokatieid_naam'>Kamer</th>
 
                 <th data-colname='topdesk.statusid_naam'>Status</th>
                 <th data-colname='topdesk.attentieid_naam'>Attentie</th>
 
                 <th data-colname='topdesk.hostnaam'>Hostnaam</th>
                 <th data-colname='topdesk.ipadres'>IP adres</th>
+                
                 <th data-colname='topdesk.macadres'>MAC adres</th>
-                <th data-colname='fixed.macadres'>MAC adres</th>
+                <th data-hide="1" data-colname='fixed.macadres'>MAC adres</th>
 
-                <th data-colname='fixed.vrijeopzoek1_naam'>Eigenaar</th>
-                <th data-colname='fixed.vrijeopzoek2_naam'>Kostenplaats</th>
-                <th data-colname='fixed.vrijetekst1'>Walloutlet</th>
+                <th data-colname='topdesk.vrijeopzoek2_naam'>Kostenplaats</th>
+                <th data-hide="1" data-colname='fixed.vrijeopzoek2_naam'>Kostenplaats</th>
+
+                <th data-colname='topdesk.vrijetekst1'>Walloutlet</th>
+                <th data-hide="1" data-colname='fixed.vrijetekst1'>Walloutlet</th>
+                
                 <th data-colname='fixed.timestamp'>Gewijzigd</th>
 
 		      </tr>
@@ -71,9 +78,19 @@ $(document).ready(function() {
         // Get modifiers from data attribute
         var mySort = [], // Initial sort
             hideThese = [], // Hidden columns
-            col = 0; // Column counter
-            columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
-
+            col = 0, // Column counter
+            columnDefs = [{ visible: false, targets: hideThese }], //Column Definitions
+            fix = function(aData, col1, where, nRow){
+                // Add warning class and add popover to column that is followed
+                // by an invisible fixed column
+                if(aData[col1+1] !== '' && aData[col1+1] !== aData[col1]){
+                    $(where, nRow)
+                        .addClass('warning')
+                        .attr('title', 'Original: '+ aData[col1])
+                        .html(aData[col1+1])
+                        .tooltip({container: 'body'});
+                }
+            }
         $('.table th').map(function(){
 
               columnDefs.push({name: $(this).data('colname'), targets: col})
@@ -112,15 +129,24 @@ $(document).ready(function() {
                 var link = get_topdesk_link(name, "<?=url('/show/item/topdesk_id/')?>" + name, options)
                 $('td:eq(0)', nRow).html(link);
                 
-                // col 7 is fixed.budgethouderid_naam
-                if(aData[7] !== '' && aData[7] !== aData[6]){
-                    $('td:eq(6)', nRow)
-                        .addClass('warning')
-                        .attr('title', 'Original: '+ aData[6])
-                        .html(aData[7])
-                        .tooltip({container: 'body'});
-                }
-
+                // col 6 is topdesk.budgethouderid_naam
+                fix(aData, 6, 'td:eq(6)', nRow);
+                
+                // col 11 is Abonnementsprijs
+                fix(aData, 11, 'td:eq(10)', nRow);
+                
+                // col 14 is Kamer
+                fix(aData, 14, 'td:eq(12)', nRow);
+                
+                // col 20 is Mac Adres
+                fix(aData, 20, 'td:eq(17)', nRow);
+                
+                // col 22 is Kostenplaats
+                fix(aData, 22, 'td:eq(18)', nRow);
+                
+                // col 24 is Walloutlet
+                fix(aData, 24, 'td:eq(19)', nRow);
+                
                 // Format date
                 var date = moment($('td:last', nRow).html(), 'X');
                 $('td:last', nRow).html(moment(date).fromNow());
