ccb4d9771dc4630bf7c345a7e44b3565c3aa7421
Arjen van Bochoven <arjen@bochoven.net>
2016-12-12 11:15:18 +0100

    Add aankoopbedrag fix in hardware view

--- app/views/fixes/hardware.php
+++ app/views/fixes/hardware.php
@@ -10,7 +10,7 @@ new Fixed;
 
   	<div class="col-lg-12">
 
-		<h3>Hardware correcties 
+		<h3>Hardware correcties
 			<span id="total-count" class='label label-primary'>…</span>
             <div class="btn-group">
               <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -44,19 +44,21 @@ new Fixed;
                 <th data-colname='topdesk.merkid_naam'>Merk</th>
                 <th data-colname='topdesk.objecttype'>Type</th>
                 <th data-colname='topdesk.serienummer'>Serienummer</th>
-                
+
                 <th data-colname='topdesk.budgethouderid_naam'>Faculteit-Dienst</th>
                 <th data-hide="1" data-colname='fixed.budgethouderid_naam'>Faculteit-Dienst</th>
 
                 <th data-colname='topdesk.leverancierid_naam'>Leverancier</th>
                 <th data-colname='topdesk.aanschafdatum'>Aanschafdatum</th>
+
                 <th data-colname='topdesk.aankoopbedrag'>Aankoopbedrag</th>
+                <th data-hide="1" data-colname='fixed.aankoopbedrag'>Aankoopbedrag</th>
 
                 <th data-colname='topdesk.vrijegetal1'>Abonnementsprijs</th>
                 <th data-hide="1" data-colname='fixed.vrijegetal1'>Abonnementsprijs</th>
 
                 <th data-colname='topdesk.vestigingid_naam'>Gebouw</th>
-                
+
                 <th data-colname='topdesk.lokatieid_naam'>Kamer</th>
                 <th data-hide="1" data-colname='fixed.lokatieid_naam'>Kamer</th>
 
@@ -67,7 +69,7 @@ new Fixed;
 
                 <th data-colname='topdesk.hostnaam'>Hostnaam</th>
                 <th data-colname='topdesk.ipadres'>IP adres</th>
-                
+
                 <th data-colname='topdesk.macadres'>MAC adres</th>
                 <th data-hide="1" data-colname='fixed.macadres'>MAC adres</th>
 
@@ -76,7 +78,7 @@ new Fixed;
 
                 <th data-colname='topdesk.vrijetekst1'>Walloutlet</th>
                 <th data-hide="1" data-colname='fixed.vrijetekst1'>Walloutlet</th>
-                
+
                 <th data-colname='fixed.timestamp'>Gewijzigd</th>
 
 		      </tr>
@@ -94,7 +96,7 @@ new Fixed;
 <script type="text/javascript">
 
 $(document).ready(function() {
-    
+
     // Add trigger to apply fixes
     $('#apply-fixes').click(function(){
         //alert('Sure?')
@@ -119,7 +121,7 @@ $(document).ready(function() {
                 console.log(request)
             }
         });
-        
+
         $.ajax({
             type: 'GET',
             url: appUrl + '/proxy/topdesk',
@@ -131,20 +133,20 @@ $(document).ready(function() {
             error: function (request, textStatus, errorThrown) {
                 if(request.status == 401){
                     // Not logged in
-                    
-                    
-                    
-                    
+
+
+
+
                 }
 
             }
         });
         // If no, show login dialog
-        
+
         // If yes: get checked changes
-        
+
         // Loop over changes, check output from Topdesk
-        
+
     })
 
     // Get modifiers from data attribute
@@ -153,7 +155,7 @@ $(document).ready(function() {
         col = 0, // Column counter
         columnDefs = [{ visible: false, targets: hideThese }], //Column Definitions
         fix = function(aData, col1, where, nRow){
-            // Add warning class and add popover to column that is followed
+            // Add success class and add popover to column that is followed
             // by an invisible fixed column
             if(aData[col1+1] !== '' && aData[col1+1] !== aData[col1]){
                 var original = aData[col1] || 'Leeg';
@@ -180,9 +182,9 @@ $(document).ready(function() {
 
           col++
     });
-    
 
-    
+
+
     oTable = $('.table').dataTable( {
         processing: true,
         stateSave: false,
@@ -196,7 +198,7 @@ $(document).ready(function() {
         initComplete: function(oSettings, json) {
 
 		  // Wrap table in responsive div
-		  $(this).wrap('<div class="table-responsive" />'); 
+		  $(this).wrap('<div class="table-responsive" />');
 
 		  // Customize search box (add clear search field button)
 		  $('.dataTables_filter label').addClass('input-group').contents().filter(function(){
@@ -206,7 +208,7 @@ $(document).ready(function() {
 		  	.attr('placeholder', 'Search')
 		  	.after($('<span style="cursor: pointer; color: #999" class="input-group-addon"><i class="fa fa-times"></i></span>')
 		  	.click(function(e){
-		  		
+
 		  		// Clear hash
 		  		var loc = window.location;
 		  		if ("replaceState" in history)
@@ -225,7 +227,7 @@ $(document).ready(function() {
 
             // Initialize responsive dropdown
             responsive_dropdown();
-            
+
             var state = 0
             var button = $('<button>')
                 .addClass('btn btn-default btn-sm')
@@ -238,7 +240,7 @@ $(document).ready(function() {
                 })
                 .append($('<span>')
                     .addClass('fa fa-check-square-o'));
-            
+
             $('#DataTables_Table_0_length')
                 .parent()
                 .prepend($('<div>')
@@ -252,7 +254,7 @@ $(document).ready(function() {
         createdRow: function( nRow, aData, iDataIndex ) {
 
             //console.log(aData)
-                            
+
             // Create topdesk link
             var name=$('td:eq(1)', nRow).html();
             var options = {}
@@ -261,7 +263,7 @@ $(document).ready(function() {
             options["<?=url('admin/topdesk_fix/')?>"+name+'/save'] = 'Fix & save';
             var link = get_topdesk_link(name, "<?=url('/show/item/topdesk_id/')?>" + name, options)
             $('td:eq(1)', nRow).html(link);
-            
+
             // Render checkbox
             var check=$('td:eq(0)', nRow).text();
             $('td:eq(0)', nRow).html(function(){
@@ -276,28 +278,40 @@ $(document).ready(function() {
                                 // reload table?
                             });
             });
-            
+
+            // function(aData, col1, where, nRow)
+
+            // aData contains all columns (visible and hidden)
+            // col1 is the column number in aData
+            // where is the column Definition (does not count hidden columns)
+            // nRow needs to be passed in.
+
+            console.log(aData);
+
             // col 6 is topdesk.budgethouderid_naam
             fix(aData, 6, 'td:eq(6)', nRow);
-            
-            // col 11 is Abonnementsprijs
-            fix(aData, 11, 'td:eq(10)', nRow);
-            
-            // col 14 is Kamer
-            fix(aData, 14, 'td:eq(12)', nRow);
-            
+
+            // col 10 is Aankoopbedrag
+            fix(aData, 10, 'td:eq(9)', nRow);
+
+            // col 12 is Abonnementsprijs
+            fix(aData, 12, 'td:eq(10)', nRow);
+
+            // col 15 is Kamer
+            fix(aData, 15, 'td:eq(12)', nRow);
+
             // col 16 is Status
-            fix(aData, 16, 'td:eq(13)', nRow);
-            
+            fix(aData, 17, 'td:eq(13)', nRow);
+
             // col 21 is Mac Adres
-            fix(aData, 21, 'td:eq(17)', nRow);
-            
+            fix(aData, 22, 'td:eq(17)', nRow);
+
             // col 23 is Kostenplaats
-            fix(aData, 23, 'td:eq(18)', nRow);
-            
+            fix(aData, 24, 'td:eq(18)', nRow);
+
             // col 25 is Walloutlet
-            fix(aData, 25, 'td:eq(19)', nRow);
-            
+            fix(aData, 26, 'td:eq(19)', nRow);
+
             // Format date
             var date = moment($('td:last', nRow).html(), 'X');
             $('td:last', nRow).html(moment(date).fromNow());
